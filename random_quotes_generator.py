b789219181585c752afdaf42a492494770b41b7e
MohammedSameeruddin <61323615+MohammedSameeruddin@users.noreply.github.com>
2021-05-21 17:46:11 +0300

    Create random_quotes_generator.py
    import random module 
    define the lists and add sample quotes or stories
    use choice function from random module
    finally print 
    
    Happy learning

---
+++ random_quotes_generator.py
@@ -0,0 +1,7 @@
+import random
+name = ['abdul kalam','Nelson Mandela','Steve Jobs','Walt Disney','Oprah Winfrey']
+quotes = ['Dream, dream, dream. Dreams transform into thoughts and thoughts result in action',
+         'The greatest glory in living lies not in never falling, but in rising every time we fall',
+         'Your time is limited, so do not waste it living someone else life. Dont be trapped by dogma – which is living with the results of other peoples thinking',
+         'If you look at what you have in life, youll always have more. If you look at what you dont have in life, youll never have enough']
+print(random.choice(name), 'said that' ,random.choice(quotes) )
